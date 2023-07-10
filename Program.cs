0b29d7ecb6873ae4141a671346c232543c0b5d8f
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-11 01:16:55 +0800

    finished

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число ");
+double number = double.Parse(Console.ReadLine());
+double variable = number / 2;
+if (variable % 1 == 0)
+{
+    Console.Write($"Число { number } целое");
+}
+else
+    Console.Write($"Число { number } не является целым");
